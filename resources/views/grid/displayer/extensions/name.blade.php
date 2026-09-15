1e0442a13d79531ee2376c5d14d4933ebda0b5e5
liyvliang <yuliang.li@poper.co>
2026-09-15 14:37:09 +0800

    DEV-11640 fix CVE-2023-33736

--- resources/views/grid/displayer/extensions/name.blade.php
+++ resources/views/grid/displayer/extensions/name.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex">
     @if($row->logo)
-        <img data-action='preview-img' src='{!! $row->logo !!}' style='max-width:40px;max-height:40px;cursor:pointer' class='img img-thumbnail' />&nbsp;&nbsp;
+        <img data-action='preview-img' src='{{ $row->logo }}' style='max-width:40px;max-height:40px;cursor:pointer' class='img img-thumbnail' />&nbsp;&nbsp;
     @endif
 
     <span class="ext-name">
